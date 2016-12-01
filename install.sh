93ffa5cd08d217e5e52d682116c2adb3ff92a061
dgeibi <png.inside@gmail.com>
2016-12-01 18:45:18 +0800

    add ibus and rime

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+
 sudo -v
-for dir in ./ct*
+for dir in ct-chez-scheme-git-archive ct-pcap-dnsproxy ct-ttf-inziu-iosevka\
+     ct-brise-git ct-ibus-git ct-librime-git ct-ibus-rime-git
 do
     cd "$dir" || exit
     rm ./*.part > /dev/null 2>&1
-    makepkg -rsicC --noconfirm --needed && cd .. || exit
+    makepkg -rsicC --needed && cd .. || exit
 done
