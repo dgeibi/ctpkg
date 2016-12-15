93e3cca53fd572e318356dc47e653bf6a3a5b829
dgeibi <png.inside@gmail.com>
2016-12-15 09:20:19 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 sudo -v
 for dir in ct-chez-scheme-git-archive ct-pcap-dnsproxy ct-ttf-inziu-iosevka\
-     ct-brise-git ct-ibus-git ct-librime-git ct-ibus-rime-git
+     ct-ibus-git ct-librime-git ct-ibus-rime-git
 do
     cd "$dir" || exit
     rm ./*.part > /dev/null 2>&1
