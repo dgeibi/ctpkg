b42f092e7dac0c4332140d62b629b728de16fbdf
dgeibi <png.inside@gmail.com>
2016-12-22 09:48:40 +0800

    remove ibus rime

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-
-sudo -v
-for dir in ct-chez-scheme-git-archive ct-pcap-dnsproxy ct-ttf-inziu-iosevka\
-     ct-ibus-git ct-librime-git ct-ibus-rime-git
+for dir in ./ct-*
 do
     cd "$dir" || exit
-    rm ./*.part > /dev/null 2>&1
+    rm -f ./*.part
     makepkg -rsicC --needed && cd .. || exit
 done
