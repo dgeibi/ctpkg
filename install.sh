0f04da0ceffab157c26d1300cac4be15c9f860cf
dgeibi <png.inside@gmail.com>
2017-03-12 14:46:34 +0800

    update

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 export iosevka_ver="$(git ls-remote -t https://github.com/be5invis/Iosevka | sort -t '/' -k 3 -V -r | grep -m 1 -oE '[0-9]+\.[0-9]+\.[0-9]+[a-z]?')"
 
-for dir in ./ct-* ./ttf-*
+for dir in ./ct-*
 do
     cd "$dir" || exit
     rm -f ./*.part
