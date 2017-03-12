8abe4877f84d37d621e360ba2c273cb388333448
dgeibi <png.inside@gmail.com>
2017-03-12 14:55:48 +0800

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export iosevka_ver="$(git ls-remote -t https://github.com/be5invis/Iosevka | sort -t '/' -k 3 -V -r | grep -m 1 -oE '[0-9]+\.[0-9]+\.[0-9]+[a-z]?')"
-
 for dir in ./ct-*
 do
     cd "$dir" || exit
